0cd3aed3a1a14e2914cc2d871a1cfe9f74163d6f
VaNiesha Honani <vhonani77@gmail.com>
2025-12-31 07:19:41 -0800

    building api and other functions before wiring up

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-nasaAPI_key = "ACpluxR905gV78LCf3SeoBcBZdjoIpRxUhENKyCt";
+const NASA_API_KEY = "ACpluxR905gV78LCf3SeoBcBZdjoIpRxUhENKyCt";
+const BASE_URL = "https://api.nasa.gov/planetary/apod";
+
+function buildApiUrl(date) {
+    const url = new URL(BASE_URL);
+    url.searchParams.append("api_key", NASA_API_KEY);
+    if (date) {
+        url.searchParams.append("date", date);
+    }
+    return url.toString();
+}
